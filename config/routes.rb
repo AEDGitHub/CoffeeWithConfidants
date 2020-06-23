62017b9c2dfce6df4ebeda49da7950ecd5b6f643
AED <arndtericdaniel@gmail.com>
2020-06-23 13:27:04 -0700

    Added backend routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
-    
+    resource :session only: [:create, :destroy]
+    resources :conurbations only: [:index] do
+      resources :confabs only: [:index, :create]
+    end
+    resources :confidants only: [:show, :create, :update, :destroy]
+    resources :confabs only: [:show, :update, :delete]
+    resources :conflations only: [:create, :destroy]
   end
 
   root "static_pages#root"
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
