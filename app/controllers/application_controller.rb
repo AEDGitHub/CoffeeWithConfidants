1cdf017d54a46f9f24f39f2af87627fb36f15d28
AED <arndtericdaniel@gmail.com>
2020-06-23 11:38:06 -0700

    [Modified application controller for authentication pattern.]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,31 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method :current_confidant, :logged_in?
+
+    private
+
+    def current_confidant
+        @current_confidant ||= Confidant.find_by(session_token: session[:session_token]) 
+    end
+
+    def require_login
+        redirect_to new_session_url unless logged_in? 
+        #I anticipate this will have to change to accomodate API namespace, ask Lina!
+    end
+
+    def login(confidant)
+        session[:session_token] = confidant.reset_session_token!
+    end
+
+    def logout
+        current_confidant.reset_session_token! if logged_in?
+        session[:session_token] = nil
+        @current_confidant = nil
+    end
+
+    def logged_in?
+        !!current_confidant
+    end
+
 end
