c47b43106998f4ad37e8ee4fcd6fb2a47f93d950
AED <arndtericdaniel@gmail.com>
2020-06-23 11:24:43 -0700

    [Modified models: Confidant, Conurbation]

--- app/models/confidant.rb
+++ app/models/confidant.rb
@@ -1,2 +1,36 @@
 class Confidant < ApplicationRecord
+    attr_reader :password
+
+    validates :username, :password_digest, :email, :session_token, prescence: true
+    validates :username, :email, :session_token, uniqueness: true
+    validates :password, length: {minimum: 7}, allow_nil: true
+
+    after_initialize :ensure_session_token
+
+    belongs_to :conurbation,
+        foreign_key: :location_id
+
+    def self.find_by_credentials(username, password)
+        confidant = Confidant.find_by(username: username)
+        confidant && confidant.is_password?(password) ? confidant : nil
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
--- app/models/conurbation.rb
+++ app/models/conurbation.rb
@@ -1,2 +1,8 @@
 class Conurbation < ApplicationRecord
+
+    validates :name, unique: true, prescence: true
+
+    has_many :confidants
+        foreign_key: :location_id
+
 end
