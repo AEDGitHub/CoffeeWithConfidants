7c3d9d00f03223c65570cf31365f1fe474870157
AED <arndtericdaniel@gmail.com>
2020-08-18 15:33:41 -0700

    Styles splash page photo and runner containers and their contents

--- frontend/components/concerto/splash/splash.jsx
+++ frontend/components/concerto/splash/splash.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Splash = () => (
     <>
-        <div className="splash-container">
+        <div className="splash-photo-container">
             <div className="splash-msg">
                 <div className="title-text">Life will change</div>
                 <div className="subtitle-text">
@@ -16,29 +16,31 @@ const Splash = () => (
                 </div>
             </div>
         </div>
-        <div className="splash-runner">
-            <div className="splash-runner-item">
-                <div className="splash-runner-item-headline">
-                    Show up to a confab
+        <div className="splash-runner-container">
+            <div className="splash-runner">
+                <div className="splash-runner-item">
+                    <div className="splash-runner-item-headline">
+                        Show up to a confab
+                    </div>
+                    <div className="splash-runner-item-subtext">
+                        You and some allies-to-be squad up to meet at a cafe.
+                    </div>
                 </div>
-                <div className="splash-runner-item-subtext">
-                    You and some allies-to-be squad up.
+                <div className="splash-runner-item">
+                    <div className="splash-runner-item-headline">
+                        Converse about anything
+                    </div>
+                    <div className="splash-runner-item-subtext">
+                        Wax poetic for two hours, no dedicated topics.
+                    </div>
                 </div>
-            </div>
-            <div className="splash-runner-item">
-                <div className="splash-runner-item-headline">
-                    Converse about anything
-                </div>
-                <div className="splash-runner-item-subtext">
-                    Wax poetic for two hours, no dedicated topics.
-                </div>
-            </div>
-            <div className="splash-runner-item">
-                <div className="splash-runner-item-headline">
-                    Observe the outcome
-                </div>
-                <div className="splash-runner-item-subtext">
-                    All done. Wasn't that fun?
+                <div className="splash-runner-item">
+                    <div className="splash-runner-item-headline">
+                        Observe the outcome
+                    </div>
+                    <div className="splash-runner-item-subtext">
+                        All done. Wasn't that fun?
+                    </div>
                 </div>
             </div>
         </div>
