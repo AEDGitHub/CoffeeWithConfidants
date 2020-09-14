cb0706f012d2a26eab8e157a400e83cf05f457e6
AED <arndtericdaniel@gmail.com>
2020-09-14 11:56:30 -0700

    Adds photo to coffeeschedule page, some styling

--- frontend/components/concerto/coffeeschedule/coffeeschedule.jsx
+++ frontend/components/concerto/coffeeschedule/coffeeschedule.jsx
@@ -4,6 +4,7 @@ destructure props coming in
 */
 
 import React from "react"
+import { Link } from "react-router-dom"
 
 class CoffeeSchedule extends React.Component {
     constructor(props) {
@@ -33,10 +34,41 @@ class CoffeeSchedule extends React.Component {
     render() {
         return (
             <>
-                <div className="coffeeschedule-photo-container">
-                    <div className="coffeeschedule-msg-container">
-                        <div className="title-text"></div>
-                        <div className="subtitle-text"></div>
+                <div className="coffeeschedule">
+                    <div className="coffeeschedule-photo-container">
+                        <div className="coffeeschedule-msg-container">
+                            <div className="title-text">GOOD CONVERSATIONS</div>
+                            <div className="subtitle-text">
+                                THEY'RE HARD TO FIND.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="coffeeschedule-content-container">
+                        <div className="coffeeschedule-msg-container">
+                            <div className="title-text">
+                                Coffee With Confidants is coffee, with
+                                confidants.
+                            </div>
+                            <div className="msg-text">
+                                Ever found yourself asking "Why does nobody want
+                                change?" This is the place to meet those who do.
+                                For two hours, squad up at a cafe with a given
+                                host to talk about finding and exposing the
+                                fakers. If none of these times work for you, why
+                                not{" "}
+                                <Link
+                                    to="/signup"
+                                    className="coffeeschedule-signup-link"
+                                >
+                                    sign up
+                                </Link>{" "}
+                                and host your own confab?
+                            </div>
+                        </div>
+                        <div className="coffeeschedule-confabs-container">
+                            <div className="month-msg"></div>
+                            <div className="confab-grid-container"></div>
+                        </div>
                     </div>
                 </div>
             </>
