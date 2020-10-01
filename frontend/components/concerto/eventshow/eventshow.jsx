30cc4853ba5e737c332768bb838e77b710eabf01
AED <arndtericdaniel@gmail.com>
2020-10-01 08:50:16 -0700

    Begins process of building out eventshow boilerplate

--- frontend/components/concerto/eventshow/eventshow.jsx
+++ frontend/components/concerto/eventshow/eventshow.jsx
@@ -7,16 +7,47 @@ class EventShow extends React.Component {
 
     // lifecycle methods
 
+    componentDidMount() {
+        if (!this.props.confabsAreLoaded) {
+            this.props.loadConfabs()
+        }
+    }
+
     // interaction handlers
 
     // displays, fields, and buttons with variable logic
 
     render() {
         // displays, fields and buttons with constant logic
+        const confabId = this.props.match.params.confabId
 
         return (
             <>
-                <div>I AM NOT ONE OF YOU, I AM NOT A HUMAN BEING</div>
+                <div className="eventshow">
+                    <div className="eventshow-main-container">
+                        <div className="eventshow-confab-column">
+                            <div className="eventshow-confab-info-container">
+                                <div className="eventshow-confab-info">
+                                    <div className="eventshow-confab-info-mainmsg"></div>
+                                    <hr></hr>
+                                </div>
+                            </div>
+                            <div className="eventshow-confab-rant"></div>
+                        </div>
+                        <div className="eventshow-confidant-column">
+                            <div className="eventshow-confidant-greeting-container">
+                                Meet your host, Ryuji!
+                            </div>
+                            <div className="eventshow-confidant-avatar-container"></div>
+                            <div className="eventshow-confidant-about-container">
+                                Hey, I'm Ryuji
+                            </div>
+                            <div className="eventshow-confidant-outro-container">
+                                No Mo Rules
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
--- frontend/components/concerto/eventshow/eventshow_container.js
+++ frontend/components/concerto/eventshow/eventshow_container.js
@@ -1,14 +1,33 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { areConfabsLoaded } from "../../../reducers/selectors"
 import { connect } from "react-redux"
+import {
+    fetchFilteredApiConfabs,
+    joinConfab,
+    leaveConfab,
+} from "../../../actions/confabs_actions"
 import EventShow from "./eventshow"
 
 const mSTP = (state) => {
-    return {}
+    return {
+        confabsAreLoaded: areConfabsLoaded(state), //todo turn this singular, regarding only the targeted confab
+        confidants: state.entities.confidants,
+        conurbations: state.entities.conurbations,
+        confabs: state.entities.confabs,
+        conflations: state.entities.conflations,
+        ccId: state.session.ccId,
+        loggedIn: Boolean(state.session.ccId),
+    }
 }
 
 const mDTP = (dispatch) => {
-    return {}
+    return {
+        loadConfabs: () => dispatch(fetchFilteredApiConfabs()), //todo turn this singular, get only required confab if not present
+        joinConfab: (confabId) => dispatch(joinConfab(confabId)),
+        leaveConfab: (confabId, conflationId) =>
+            dispatch(leaveConfab(confabId, conflationId)),
+    }
 }
 
 export default connect(mSTP, mDTP)(EventShow)
