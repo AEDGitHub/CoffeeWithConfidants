b527dc7beaf7a7aa11b3f5cfaed7f4cbbf59bf75
AED <arndtericdaniel@gmail.com>
2020-12-05 16:44:47 -0800

    Finishes conversion of sessionform to functional component, pending testing

--- frontend/components/concerto/sessionform/sessionform.jsx
+++ frontend/components/concerto/sessionform/sessionform.jsx
@@ -1,172 +1,158 @@
 import React, { useState, useEffect } from "react"
 
 function SessionForm({
-   conurbations,
-   conurbationsAreLoaded,
-   demoConfidantConurbationId,
-   formType,
-   mainMsg,
-   subMsg,
-   unPlaceholder,
-   pwPlaceholder,
-   submitButtonText,
-   navLink,
-   loadConurbations,
-   unloadConurbations,
-   processMainForm,
-   processDemoForm,
+	conurbations,
+	conurbationsAreLoaded,
+	demoConfidantConurbationId,
+	formType,
+	mainMsg,
+	subMsg,
+	unPlaceholder,
+	pwPlaceholder,
+	submitButtonText,
+	navLink,
+	loadConurbations,
+	unloadConurbations,
+	processMainForm,
+	processDemoForm,
 }) {
-   const [username, setUsername] = useState("")
-   const [email, setEmail] = useState("")
-   const [password, setPassword] = useState("")
-   const [locationId, setLocationId] = useState(null)
-   const [demoUserCityId, setDemoUserCityId] = useState(null)
+	const [username, setUsername] = useState("")
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [locationId, setLocationId] = useState(null)
+	const [demoUserCityId, setDemoUserCityId] = useState(null)
 
-   useEffect(()=>{
-      if (formType === "signup") {
-         loadConurbations()
-      }
-      return function cleanup(){
-         unloadConurbations()
-      }
-   }, [])
+	useEffect(() => {
+		if (formType === "signup") {
+			loadConurbations()
+		}
+		return function cleanup() {
+			unloadConurbations()
+		}
+	}, [])
 
-	handleSubmit(e) {
+	const handleSubmit = (e) => {
 		e.preventDefault()
 		const confidant = {
-			username: this.state.username,
-			email: this.state.email,
-			password: this.state.password,
-			location_id: this.state.locationId,
+			username: username,
+			email: email,
+			password: password,
+			location_id: locationId,
 		}
-		this.props.processMainForm(confidant)
+		processMainForm(confidant)
 	}
 
-	handleDemoSubmit(e) {
+	const handleDemoSubmit = (e) => {
 		e.preventDefault()
 		const demoConfidant = {
 			username: "Joker",
 			password: "hunter12",
 		}
-
-		this.props.processDemoForm(demoConfidant)
+		processDemoForm(demoConfidant)
 	}
 
-	emailField() {
-		if (this.props.formType === "signin") {
-			return <></>
-		} else {
-			return (
-				<input
+	const emailField = () =>
+		formType === "signin" ? (
+			<></>
+		) : (
+			<input
+				required={true}
+				type="email"
+				className="sessionform-form-field"
+				onChange={(e) => setEmail(e.target.value)}
+				placeholder="Email address"
+				value={email}
+			/>
+		)
+
+	const homeCityField = () =>
+		formType === "signin" ? (
+			<></>
+		) : (
+			<div className="sessionform-dropdown">
+				<select
 					required={true}
-					type="email"
-					className="sessionform-form-field"
-					onChange={this.update("email")}
-					placeholder="Email address"
-					value={this.state.email}
-				/>
-			)
-		}
-	}
+					defaultValue="Home conurbation"
+					onChange={(e) => setLocationId(e.target.value)}
+				>
+					<option disabled="disabled" value="Home conurbation">
+						Home conurbation
+					</option>
+					{homeCityFieldOptions()}
+				</select>
+			</div>
+		)
 
-	homeCityField() {
-		if (this.props.formType === "signin") {
-			return <></>
-		} else {
-			return (
-				<div className="sessionform-dropdown">
-					<select
-						required={true}
-						defaultValue="Home conurbation"
-						onChange={this.update("locationId")}
-					>
-						<option disabled="disabled" value="Home conurbation">
-							Home conurbation
-						</option>
-						{this.homeCityFieldOptions()}
-					</select>
-				</div>
-			)
-		}
-	}
-
-	homeCityFieldOptions() {
-		return this.props.conurbations.map((conurbation, idx) => (
+	const homeCityFieldOptions = () =>
+		conurbations.map((conurbation, idx) => (
 			<option value={conurbation.id} key={idx}>
 				{conurbation.name}
 			</option>
 		))
-	}
 
-	render() {
-		const mainMsgDisplay = (
-			<div className="sessionform-main-msg">{this.props.mainMsg}</div>
-		)
+	const mainMsgDisplay = <div className="sessionform-main-msg">{mainMsg}</div>
 
-		const subMsgDisplay = (
-			<div className="sessionform-sub-msg">{this.props.subMsg}</div>
-		)
+	const subMsgDisplay = <div className="sessionform-sub-msg">{subMsg}</div>
 
-		const usernameField = (
-			<input
-				required={true}
-				type="text"
-				className="sessionform-form-field"
-				onChange={this.update("username")}
-				placeholder={this.props.unPlaceholder}
-				value={this.state.username}
-			/>
-		)
+	const usernameField = (
+		<input
+			required={true}
+			type="text"
+			className="sessionform-form-field"
+			onChange={(e) => setUsername(e.target.value)}
+			placeholder={unPlaceholder}
+			value={username}
+		/>
+	)
 
-		const passwordField = (
-			<input
-				required={true}
-				type="password"
-				className="sessionform-form-field"
-				onChange={this.update("password")}
-				placeholder={this.props.pwPlaceholder}
-				value={this.state.password}
-			/>
-		)
+	const passwordField = (
+		<input
+			required={true}
+			type="password"
+			className="sessionform-form-field"
+			onChange={(e) => {
+				setPassword(e.target.value)
+			}}
+			placeholder={pwPlaceholder}
+			value={password}
+		/>
+	)
 
-		const submitFormButton = (
-			<input
-				type="submit"
-				className="sessionform-submit-button"
-				value={this.props.submitButtonText}
-			/>
-		)
+	const submitFormButton = (
+		<input
+			type="submit"
+			className="sessionform-submit-button"
+			value={submitButtonText}
+		/>
+	)
 
-		const demoUserButton = (
-			<input
-				type="submit"
-				className="sessionform-submit-button"
-				value="SIGN IN DEMO USER"
-			/>
-		)
+	const demoUserButton = (
+		<input
+			type="submit"
+			className="sessionform-submit-button"
+			value="SIGN IN DEMO USER"
+		/>
+	)
 
-		return (
-			<>
-				<div className="sessionform">
-					<div className="sessionform-form-container">
-						{mainMsgDisplay}
-						{subMsgDisplay}
-						<form onSubmit={this.handleSubmit}>
-							{usernameField}
-							{this.emailField()}
-							{passwordField}
-							{this.homeCityField()}
-							{submitFormButton}
-						</form>
-						<form onSubmit={this.handleDemoSubmit}>
-							{demoUserButton}
-						</form>
-						{this.props.navLink}
-					</div>
+	return (
+		<>
+			<div className="sessionform">
+				<div className="sessionform-form-container">
+					{mainMsgDisplay()}
+					{subMsgDisplay()}
+					<form onSubmit={handleSubmit}>
+						{usernameField()}
+						{emailField()}
+						{passwordField()}
+						{homeCityField()}
+						{submitFormButton()}
+					</form>
+					<form onSubmit={handleDemoSubmit}>{demoUserButton()}</form>
+					{navLink}
 				</div>
-			</>
-		)
-	}
+			</div>
+		</>
+	)
 }
 
 //class component, antiquated
@@ -208,7 +194,7 @@ function SessionForm({
 //    handleSubmit(e) {
 //       e.preventDefault()
 //       const confidant = {
-//          username: this.state.username,
+//          username:this.state.username,
 //          email: this.state.email,
 //          password: this.state.password,
 //          location_id: this.state.locationId,
