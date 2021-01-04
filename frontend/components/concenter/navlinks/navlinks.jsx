4faa476d5f8a7cdcf676e98fca5c396ba5121655
AED <arndtericdaniel@gmail.com>
2021-01-03 18:07:44 -0800

    Reorganizes NavLinks component

--- frontend/components/concenter/navlinks/navlinks.jsx
+++ frontend/components/concenter/navlinks/navlinks.jsx
@@ -1,40 +1,39 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-const NavLinks = ({ currentConfidant, logout }) => {
-	const generateLink = (
-		linkClass,
-		url,
-		displayText,
-		onClickFunction = null
-	) => (
-		<Link
-			className={`link-${linkClass}`}
-			to={url}
-			onClick={onClickFunction}
-		>
-			{displayText}
-		</Link>
-	)
-
-	const variableLinks = currentConfidant ? (
-		<>
-			{generateLink("normal", "/confidants/edit", "CONTROL")}
-			{generateLink("normal", "/", "SIGN OUT", logout)}
-		</>
-	) : (
-		<>
-			{generateLink("normal", "/signin", "SIGN IN")}
-			{generateLink("signup", "/signup", "SIGN UP")}
-		</>
-	)
-
+function NavLinks({ currentConfidant, logout }) {
 	return (
 		<nav>
 			{generateLink("normal", "/coffee_times", "COFFEE TIMES")}
-			{variableLinks}
+			{variableLinks()}
 		</nav>
 	)
+
+	function generateLink(linkClass, url, displayText, onClickFunction = null) {
+		return (
+			<Link
+				className={`link-${linkClass}`}
+				to={url}
+				onClick={onClickFunction}
+			>
+				{displayText}
+			</Link>
+		)
+	}
+
+	function variableLinks() {
+		return currentConfidant ? (
+			<>
+				{generateLink("normal", "/confidants/edit", "CONTROL")}
+				{generateLink("normal", "/", "SIGN OUT", logout)}
+			</>
+		) : (
+			<>
+				{generateLink("normal", "/signin", "SIGN IN")}
+				{generateLink("signup", "/signup", "SIGN UP")}
+			</>
+		)
+	}
 }
 
 export default NavLinks
